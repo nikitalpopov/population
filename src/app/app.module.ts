ffe1d52624a4078148c8b7f4125f305aa48d8b03
Nikita Popov <mailto.nikitapopov@gmail.com>
2021-04-10 18:24:38 +0200

    Update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppComponent } from '@app/app.component';
+import { MaterialModule } from '@app/material.module';
 import { WorldMapComponent } from '@components/world-map/world-map.component';
 
 @NgModule({
@@ -13,9 +16,14 @@ import { WorldMapComponent } from '@components/world-map/world-map.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
 
-    LeafletModule
+    LeafletModule,
+
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { A11yModule } from '@angular/cdk/a11y';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PortalModule } from '@angular/cdk/portal';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [
+    A11yModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    PortalModule,
+    ScrollingModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule
+  ]
+})
+export class MaterialModule { }
